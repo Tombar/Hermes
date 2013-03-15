f038d49f804c542a237223ce9fba4f1338be2a5b
Martin Loy <martin@guruhub.com.uy>
2013-03-15 03:23:35 -0300

    my fish settings

--- hermes/config/fish/config.fish
+++ hermes/config/fish/config.fish
@@ -1,8 +1,8 @@
 if status --is-login
 
-  for p in /usr/local/bin /usr/local/sbin /usr/local/share/npm/bin /opt/local/bin ~/.hermes/hermes/bin
+  for p in /usr/local/bin /usr/local/sbin /opt/local/bin ~/.hermes/hermes/bin /usr/local/sbin 
     if test -d $p
-      set PATH $p $PATH
+      set -x PATH $p $PATH
     end
   end
 
@@ -15,7 +15,6 @@ set -x CLICOLOR 1
 set -u EDITOR /usr/local/bin/vim
 set -u NODE_PATH /usr/local/lib/node
 
-rvm reload > /dev/null
 autojump   > /dev/null
 
 function rehash
@@ -29,25 +28,31 @@ end
 function fish_prompt -d "Write out the prompt"
   set -l last_status $status
 
+  # Print last fg task exit status
+  switch $last_status
+    case 0
+        # do nothing
+    case '*'
+        printf '%sExit code %s for last command \n' (set_color red) (echo $last_status)
+  end
+
+  printf '%s%s@%s:' (set_color cyan) (whoami) (hostname)
   # color writeable dirs green, read-only dirs red
   if test -w "."
-    printf '%s%s' (set_color brown) (prompt_pwd)
+    printf '%s%s' (set_color brown) (pwd)
   else
-    printf '%s%s' (set_color red) (prompt_pwd)
+    printf '%s%s' (set_color red) (pwd)
   end
 
   # Print git branch
   if test -d ".git"
-    prompt_git
+    printf ' %s' (parse_git_branch)
   end
 
   # Print RVM info
-  printf ' %s%s' (set_color green) (rvm-prompt v)
-
-  # Print last fg task exit status
-  printf ' %s%s' (set_color red) (echo $last_status)
+  printf ' %s%s ' (set_color green) (~/.rvm/bin/rvm-prompt v g)
 
   # Separator and reset
-  printf '%s ± %s' (set_color brown) (set_color normal)
+  printf '%s$ %s' (set_color brown) (set_color normal)
 
 end
